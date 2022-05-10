033e0e8119297fc9e3049212ba7ffaa3429376cd
Abu! <100409086+Afx-Abu@users.noreply.github.com>
2022-05-10 07:27:43 +0530

    🤩 𝙰𝙻𝙴𝚇𝙰 𝙽𝙴𝚆 𝚄𝙿𝙳𝙰𝚃𝙴 💯

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -5,15 +5,16 @@ const ffmpeg = require('fluent-ffmpeg');
 const {execFile} = require('child_process');
 const cwebp = require('cwebp-bin');
 const Config = require('../config');
+const {skbuffer} = require('../buffer');
 
 const Language = require('../language');
 const Lang = Language.getString('sticker');
 
 if (Config.WORKTYPE == 'private') {
     Alexa.addCommand({pattern: 'sticker$', fromMe: true, deleteCommand: true, desc: Lang.STICKER_DESC}, (async (message, match) => {    
-
-        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
-        var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
+    var img = await skbuffer(Config.LOGOSK)
+        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { participant : '0@s.whatsapp.net'},message: {orderMessage: {itemCount : 990,status: 1,surface : 1,message: config.BOT,orderTitle: `THIS IS NEW?`,thumbnail: img, sellerJid: Config.JID }}}});
+        var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { participant : '0@s.whatsapp.net'},message: {orderMessage: {itemCount : 990,status: 1,surface : 1,message: config.BOT,orderTitle: `THIS IS NEW?`,thumbnail: img, sellerJid: Config.JID }}}});
         var location = await message.client.downloadAndSaveMediaMessage({
             key: {
                 remoteJid: message.reply_message.jid,
@@ -28,7 +29,7 @@ if (Config.WORKTYPE == 'private') {
                 .videoFilters('scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
                 .save('st.webp')
                 .on('end', async () => {
-                    await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker);
+                    await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { participant : '0@s.whatsapp.net'},message: {orderMessage: {itemCount : 990,status: 1,surface : 1,message: config.BOT,orderTitle: `THIS IS NEW?`,thumbnail: img, sellerJid: Config.JID }}}});
             });
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 
@@ -39,7 +40,7 @@ if (Config.WORKTYPE == 'private') {
             .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
             .save('sticker.webp')
             .on('end', async () => {
-                await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
+                await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { participant : '0@s.whatsapp.net'},message: {orderMessage: {itemCount : 990,status: 1,surface : 1,message: config.BOT,orderTitle: `THIS IS NEW?`,thumbnail: img, sellerJid: Config.JID }}}});
             });
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
@@ -47,9 +48,9 @@ if (Config.WORKTYPE == 'private') {
 else if (Config.WORKTYPE == 'public') {
 
     Alexa.addCommand({pattern: 'sticker$', fromMe: false, deleteCommand: false, desc: Lang.STICKER_DESC}, (async (message, match) => {    
-
-        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
-        var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
+    var img = await skbuffer(Config.LOGOSK)
+        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { participant : '0@s.whatsapp.net'},message: {orderMessage: {itemCount : 990,status: 1,surface : 1,message: config.BOT,orderTitle: `THIS IS NEW?`,thumbnail: img, sellerJid: Config.JID }}}});
+        var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { participant : '0@s.whatsapp.net'},message: {orderMessage: {itemCount : 990,status: 1,surface : 1,message: config.BOT,orderTitle: `THIS IS NEW?`,thumbnail: img, sellerJid: Config.JID }}}});
         var location = await message.client.downloadAndSaveMediaMessage({
             key: {
                 remoteJid: message.reply_message.jid,
@@ -64,7 +65,7 @@ else if (Config.WORKTYPE == 'public') {
                 .videoFilters('scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
                 .save('st.webp')
                 .on('end', async () => {
-                    await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker);
+                    await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { participant : '0@s.whatsapp.net'},message: {orderMessage: {itemCount : 990,status: 1,surface : 1,message: config.BOT,orderTitle: `THIS IS NEW?`,thumbnail: img, sellerJid: Config.JID }}}});
             });
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid,contextInfo: { forwardingScore: 2, isForwarded: true }, fromMe: true})
 
@@ -75,7 +76,7 @@ else if (Config.WORKTYPE == 'public') {
             .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
             .save('sticker.webp')
             .on('end', async () => {
-                await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
+                await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { participant : '0@s.whatsapp.net'},message: {orderMessage: {itemCount : 990,status: 1,surface : 1,message: config.BOT,orderTitle: `THIS IS NEW?`,thumbnail: img, sellerJid: Config.JID }}}});
             });
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid,contextInfo: { forwardingScore: 2, isForwarded: true }, fromMe: true})
     }));
